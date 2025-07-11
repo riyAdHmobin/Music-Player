9c9eb9f8d7f3baf9b516a49636a13ea24460ccc2
riyadhm0bin <mobinul.riyadh@polygontech.xyz>
2025-07-11 20:41:14 +0600

    name changed

--- assets/music-list/tracks.js
+++ assets/music-list/tracks.js
@@ -2,7 +2,4 @@ const tracks = [
     { id: "kfKP4o-bXzM", title: "Kaavish - Tere Pyaar Main", duration: null },
     { id: "9sekgEXGm-E", title: "Coke Studio Season 10| Faasle | Kaavish & Quratulain Balouch", duration: null },
     { id: "YMz4y8rQ4kU", title: "Kaavish - Tairay Naam", duration: null },
-    { id: "TK4N5W22Gts", title: "2 Second Video 1", duration: null },
-    { id: "TK4N5W22Gts", title: "2 Second Video 2", duration: null },
-    { id: "TK4N5W22Gts", title: "2 Second Video 3", duration: null },
 ];
